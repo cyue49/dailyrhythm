8dbc4aa4bcfb482509d6241b7a95ddaa443263fa
Chen Yue <yuechen049@gmail.com>
2024-07-31 22:40:38 -0400

    added routes for updating a user's username, email, password, verified status, and image url

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,6 +13,7 @@ router.get('/', async (req, res) => {
         pool.query('SELECT * FROM users', (err, result) => {
             if (err) {
                 console.log('Error executing query.', err);
+                res.status(400).send('failed');
             } else {
                 // send response
                 console.log(result.rows);
@@ -32,6 +33,7 @@ router.get('/id/:id', async (req, res) => {
         pool.query('SELECT * FROM users WHERE user_id = $1', [req.params.id], (err, result) => {
             if (err) {
                 console.log('Error executing query.', err);
+                res.status(400).send('failed');
             } else {
                 // send response
                 console.log(result.rows[0]);
@@ -51,6 +53,7 @@ router.get('/email/:email', async (req, res) => {
         pool.query('SELECT * FROM users WHERE email = $1', [req.params.email], (err, result) => {
             if (err) {
                 console.log('Error executing query.', err);
+                res.status(400).send('failed');
             } else {
                 // send response
                 console.log(result.rows[0]);
@@ -70,6 +73,7 @@ router.get('/verified', async (req, res) => {
         pool.query('SELECT * FROM users WHERE is_verified = true', (err, result) => {
             if (err) {
                 console.log('Error executing query.', err);
+                res.status(400).send('failed');
             } else {
                 // send response
                 console.log(result.rows);
@@ -89,6 +93,7 @@ router.get('/notverified', async (req, res) => {
         pool.query('SELECT * FROM users WHERE is_verified = false', (err, result) => {
             if (err) {
                 console.log('Error executing query.', err);
+                res.status(400).send('failed');
             } else {
                 // send response
                 console.log(result.rows);
@@ -108,6 +113,7 @@ router.get('/id/:id/settings', async (req, res) => {
         pool.query('SELECT * FROM settings WHERE user_id = $1', [req.params.id], (err, result) => {
             if (err) {
                 console.log('Error executing query.', err);
+                res.status(400).send('failed');
             } else {
                 // send response
                 console.log(result.rows[0]);
@@ -144,12 +150,14 @@ router.post('/', async (req, res) => {
             (err, result) => {
                 if (err) {
                     console.log('Error creating new user.', err);
+                    res.status(400).send('failed');
                 } else {
                     // create and insert new settings for user into db
                     pool.query('INSERT INTO settings (settings_id, user_id) VALUES ($1, $2)', [settings_id, user_id],
                         (err1, result1) => {
                             if (err1) {
                                 console.log('Error creating new settings.', err);
+                                res.status(400).send('failed');
                             } else {
                                 // send response
                                 res.status(200).send('success');
@@ -165,7 +173,135 @@ router.post('/', async (req, res) => {
 });
 
 // ============================================= PUT =============================================
+// update a user's username
+router.put('/id/:id/username', async (req, res) => {
+    // input validation
+    const { error } = validateUpdate(req.body, 'username')
+    if (error) return res.status(400).send(error.details[0].message);
 
+    try {
+        // value to update
+        const { username } = req.body;
+
+        // update in the database
+        pool.query('UPDATE users SET username = $1 WHERE user_id = $2', [username, req.params.id], (err, result) => {
+            if (err) {
+                console.log('Error updating username.', err);
+                res.status(400).send('failed');
+            } else {
+                // send response
+                res.status(200).send('success');
+            }
+        })
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).send(e.message);
+    }
+});
+
+// update a user's profile image url
+router.put('/id/:id/imageurl', async (req, res) => {
+    // input validation
+    const { error } = validateUpdate(req.body, 'imageurl')
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+        // value to update
+        const { image_url } = req.body;
+
+        // update in the database
+        pool.query('UPDATE users SET image_url = $1 WHERE user_id = $2', [image_url, req.params.id], (err, result) => {
+            if (err) {
+                console.log('Error updating image url.', err);
+                res.status(400).send('failed');
+            } else {
+                // send response
+                res.status(200).send('success');
+            }
+        })
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).send(e.message);
+    }
+});
+
+// update a user's email
+router.put('/id/:id/email', async (req, res) => {
+    // input validation
+    const { error } = validateUpdate(req.body, 'email')
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+        // value to update
+        const { email } = req.body;
+
+        // update in the database
+        pool.query('UPDATE users SET email = $1 WHERE user_id = $2', [email, req.params.id], (err, result) => {
+            if (err) {
+                console.log('Error updating image url.', err);
+                res.status(400).send('failed');
+            } else {
+                // send response
+                res.status(200).send('success');
+            }
+        })
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).send(e.message);
+    }
+});
+
+// update a user's password
+router.put('/id/:id/password', async (req, res) => {
+    // input validation
+    const { error } = validateUpdate(req.body, 'password')
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+        // value to update
+        const { user_password } = req.body;
+
+        // update in the database
+        pool.query('UPDATE users SET user_password = $1 WHERE user_id = $2', [user_password, req.params.id], (err, result) => {
+            if (err) {
+                console.log('Error updating image url.', err);
+                res.status(400).send('failed');
+            } else {
+                // send response
+                res.status(200).send('success');
+            }
+        })
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).send(e.message);
+    }
+});
+
+// update a user's is verified
+router.put('/id/:id/verified', async (req, res) => {
+    // input validation
+    const { error } = validateUpdate(req.body, 'verified')
+    if (error) return res.status(400).send(error.details[0].message);
+
+    try {
+        // value to update
+        const { is_verified } = req.body;
+
+        // update in the database
+        pool.query('UPDATE users SET is_verified = $1 WHERE user_id = $2', [is_verified, req.params.id], (err, result) => {
+            if (err) {
+                console.log('Error updating image url.', err);
+                res.status(400).send('failed');
+            } else {
+                // send response
+                res.status(200).send('success');
+            }
+        })
+    } catch (e) {
+        console.log(e.message);
+        res.status(400).send(e.message);
+    }
+});
 
 // ============================================= DELETE =============================================
 
--- backend/validations/users.js
+++ backend/validations/users.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const email = Joi.string().pattern(/^[\w\.\-]+@([\w\-]+\.)+[\w\-]{2,4}$/);
 const username = Joi.string().pattern(/^[\w\s\-]{1,30}$/);
 const user_password = Joi.string().pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/);
+const hashed_password = Joi.string();
 const is_verified = Joi.boolean();
 const image_url = Joi.string();
 
@@ -16,12 +17,41 @@ function validateNewUser(user) {
     return userSchema.validate(user);
 }
 
-function validateUpdateUser(user) {
-    const userSchema = Joi.object().keys({
-        email, username, user_password, is_verified, image_url
+function validateUpdateUser(user, field) {
+    const updateUsernameSchema = Joi.object().keys({
+        username: username.required()
     });
 
-    return userSchema.validate(user);
+    const updateEmailSchema = Joi.object().keys({
+        email: email.required()
+    });
+
+    const updatePasswordSchema = Joi.object().keys({
+        user_password: hashed_password.required()
+    });
+
+    const updateVerifiedSchema = Joi.object().keys({
+        is_verified: is_verified.required()
+    });
+
+    const updateImageUrlSchema = Joi.object().keys({
+        image_url: image_url.required()
+    });
+
+    switch (field) {
+        case 'username':
+            return updateUsernameSchema.validate(user);
+        case 'email':
+            return updateEmailSchema.validate(user);
+        case 'password':
+            return updatePasswordSchema.validate(user);
+        case 'verified':
+            return updateVerifiedSchema.validate(user);
+        case 'imageurl':
+            return updateImageUrlSchema.validate(user);
+        default:
+            throw new Error("Error: invalid field");
+    }
 }
 
 exports.validateNew = validateNewUser;
