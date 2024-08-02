e45766f2b2a1b21926a1734808103f4a3a7e3fec
Chen Yue <yuechen049@gmail.com>
2024-08-02 01:54:55 -0400

    updated Sign In page and Homepage ui

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <div className='h-screen w-screen p-8 center-of-div flex-col gap-8 bg-appWhite'>
+        <div className='h-screen w-screen p-8 center-of-div flex-col gap-8 text-appBlack bg-appWhite'>
             <img
                 className='w-20 h-20'
                 src={require(`../assets/applogo.png`)}
@@ -11,9 +12,9 @@ const Home = () => {
 
             <h3 className='text-2xl font-bold font-sans text-appGreen'>Daily Rhythm</h3>
 
-            <p className='text-center w-5/6 md:w-2/4'>Daily Rhythm helps you track, maintain, and manage daily habits and routines towards a healthy life rhythm. </p>
+            <p className='text-center w-5/6 md:w-2/4 max-w-md'>Daily Rhythm helps you track, maintain, and manage daily habits and routines towards a healthy life rhythm. </p>
 
-            <button className='primary-green-button hover:secondary-green-button focus:secondary-green-button mt-2'>Get Started!</button>
+            <Link to={'/signin'} className='primary-green-button hover:secondary-green-button focus:secondary-green-button mt-2'>Get Started!</Link>
         </div>
     )
 }
--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignIn = () => {
     return (
-        <div>
-            Sign in page
+        <div className='h-screen w-screen min-w-screen min-h-screen center-of-div bg-appGreen text-appBlack'>
+            <div className='w-10/12 h-fit max-w-[450px] max-h-fit min-w-80 min-h-96 p-8 rounded-3xl bg-appGray-1 flex flex-col'>
+                <img
+                    className='w-10 h-10 self-center'
+                    src={require(`../assets/applogo.png`)}
+                    alt="app logo"
+                />
+                <p className='text-3xl font-bold font-sans my-6'>Sign In</p>
+                <form className='flex flex-col'>
+                    <label className='form-label'>Email: </label>
+                    <input className='form-text-input' type='text' name='email' />
+                    <label className='form-label'>Password: </label>
+                    <input className='form-text-input' type='password' name='user_password' />
+                    <input className='primary-green-button hover:secondary-green-button mt-8' type="submit" value="Sign In" />
+                </form>
+                <div className='text-sm text-center mt-4 mb-8 hover:underline hover:text-appGreen'>
+                    <Link to={'/signup'}>Don't have an account? Sign up instead!</Link>
+                </div>
+
+            </div>
         </div>
     )
 }
