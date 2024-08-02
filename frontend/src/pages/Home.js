85ebe6d1aa904fd41f444db008202e736350f9fe
Chen Yue <yuechen049@gmail.com>
2024-08-02 00:27:27 -0400

    updated Homepage ui

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            Home Page
+        <div className='h-screen w-screen p-8 center-of-div flex-col gap-8 bg-appWhite'>
+            <img
+                className='w-20 h-20'
+                src={require(`../assets/applogo.png`)}
+                alt="app logo"
+            />
+
+            <h3 className='text-2xl font-bold font-sans text-appGreen'>Daily Rhythm</h3>
+
+            <p className='text-center w-5/6 md:w-2/4'>Daily Rhythm helps you track, maintain, and manage daily habits and routines towards a healthy life rhythm. </p>
+
+            <button className='primary-green-button hover:secondary-green-button focus:secondary-green-button mt-2'>Get Started!</button>
         </div>
     )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                appGreen: '#528E6C',
+                appBlack: '#353535',
+                appWhite: '#F3F3F3',
+                appRed: '#AE5A5A',
+                appGray: {
+                    1: '#DBDBDB',
+                    2: '#B9B9B9',
+                    3: '#858585'
+                }
+            }
+        },
+        fontFamily: {
+            'sans': 'Helvetica, Arial, sans-serif'
+        }
+    },
+    plugins: [],
 }
 
