e11f6aab8749ed6801f7ee00a13485ea7bd081b6
Chen Yue <yuechen049@gmail.com>
2024-08-06 23:39:12 -0400

    added form validation for User Card on Profile page

--- frontend/src/components/profile/UserCard.js
+++ frontend/src/components/profile/UserCard.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faPenToSquare, faCircleCheck } from '@fortawesome/free-solid-svg-icons'
 
 const UserCard = ({ user }) => {
     const [isEdit, setIsEdit] = useState(false)
     const [form, setForm] = useState({ username: 'Chen Yue', email: 'yuechen049@gmail.com' })
+    const [validEmail, setValidEmail] = useState(false)
+    const [validUsername, setValidUsername] = useState(false)
+
+    const validateInputs = useCallback(() => {
+        // validations & regex
+        const emailRe = new RegExp(/^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/)
+        const usernameRe = new RegExp(/^[\w\s-]{1,50}$/)
+
+        if (emailRe.test(form.email)) {
+            setValidEmail(true)
+        } else {
+            setValidEmail(false)
+        }
+        if (usernameRe.test(form.username)) {
+            setValidUsername(true)
+        } else {
+            setValidUsername(false)
+        }
+    }, [form]);
+
+    useEffect(() => {
+        validateInputs()
+    }, [validateInputs]);
 
     const handleChange = (e) => {
         setForm({
@@ -37,6 +60,7 @@ const UserCard = ({ user }) => {
                         <div className='flex flex-col w-10/12'>
                             <div className='font-bold'>Username:</div>
                             <input className='form-text-input w-full truncate' type='text' name='username' id='username' autoComplete='on' autoCapitalize='off' placeholder='Username' value={form.username} onChange={handleChange} />
+                            <div className={`text-appRed text-sm ${(form.username !== '' && !validUsername) ? '' : 'hidden'}`}>Invalid character in username.</div>
                         </div>
 
                     }
@@ -52,6 +76,7 @@ const UserCard = ({ user }) => {
                         <div className='flex flex-col w-10/12'>
                             <div className='font-bold'>Email:</div>
                             <input className='form-text-input w-full truncate' type='text' name='email' id='email' autoComplete='on' autoCapitalize='off' placeholder='Email' value={form.email} onChange={handleChange} />
+                            <div className={`text-appRed text-sm ${(form.email !== '' && !validEmail) ? '' : 'hidden'}`}>Invalid email.</div>
                         </div>
                     }
                 </div>
