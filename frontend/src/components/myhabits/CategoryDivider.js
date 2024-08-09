a80a8338be894b89cda1c2f6c4685338a3bdfa58
Chen Yue <yuechen049@gmail.com>
2024-08-08 23:11:45 -0400

    added fetch all habits for each category

--- frontend/src/components/myhabits/CategoryDivider.js
+++ frontend/src/components/myhabits/CategoryDivider.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import HabitCard from './HabitCard'
 
 const CategoryDivider = ({ category }) => {
+    const [habits, setHabits] = useState([])
+
+    // fetch all user habits for this category
+    const getCategories = useCallback(() => {
+        fetch(`http://127.0.0.1:5000/api/custom_habits/active/${category.category_id}`, { credentials: 'include' })
+            .then((res) => {
+                if (res.status === 200 && res.ok) {
+                    res.json()
+                        .then((data) => {
+                            setHabits(data)
+                        })
+                }
+            })
+            .catch((e) => {
+                console.log(e.message)
+            })
+    }, []);
+
+    useEffect(() => {
+        getCategories()
+    }, [getCategories]);
+
     return (
         <div className='center-of-div flex-col flex-nowrap w-full gap-1'>
             <div className='flex flex-row flex-nowrap w-full justify-center items-center gap-3'>
                 <div className='text-appGreen font-bold'>{category.category_name}</div>
                 <div className='center-of-div h-[2px] bg-appGreen flex-1'> </div>
-                <div className='text-appGreen font-bold'>2</div>
+                <div className='text-appGreen font-bold'>{habits.length}</div>
             </div>
-            <HabitCard />
+            {habits.map((habit, index) => (
+                <HabitCard habit={habit} key={index} />
+            ))}
         </div>
 
     )
--- frontend/src/components/myhabits/HabitCard.js
+++ frontend/src/components/myhabits/HabitCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const HabitCard = () => {
+const HabitCard = ({ habit }) => {
     return (
         <div className='w-full flex flex-row flex-nowrap items-center justify-between px-3 py-2 gap-4 rounded-full bg-appGray-1'>
             <div className='rounded-full border border-appGreen bg-appWhite px-3 py-1'>2</div>
-            <div className='flex-1 truncate'>Read a chapter of a book</div>
+            <div className='flex-1 truncate'>{habit.habit_name}</div>
             <div className='text-xs text-appGray-3 pr-2 font-bold'>Total: 16</div>
         </div>
     )
