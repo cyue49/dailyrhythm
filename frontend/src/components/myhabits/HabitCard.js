ae64dc2711aa0ced1a6c74d404f0f0ff0a054c48
Chen Yue <yuechen049@gmail.com>
2024-08-09 01:04:46 -0400

    added creating a new checkin when clicking on the daily count circle

--- frontend/src/components/myhabits/HabitCard.js
+++ frontend/src/components/myhabits/HabitCard.js
@@ -10,13 +10,13 @@ const HabitCard = ({ habit, currentDay }) => {
         const offset = currentDay.getTimezoneOffset()
         const newDay = new Date(currentDay.getTime() - (offset * 60 * 1000)).toISOString().split('T')[0]
 
-        // fetch count
+        // fetch single day checkin count
         fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit.habit_id}/count/day/${newDay}`, { credentials: 'include' })
             .then((res) => {
                 if (res.status === 200 && res.ok) {
                     res.json()
                         .then((data) => {
-                            setDailyCount(data.count)
+                            setDailyCount(parseInt(data.count))
                         })
                 }
             })
@@ -37,7 +37,7 @@ const HabitCard = ({ habit, currentDay }) => {
                 if (res.status === 200 && res.ok) {
                     res.json()
                         .then((data) => {
-                            setTotalCount(data.count)
+                            setTotalCount(parseInt(data.count))
                         })
                 }
             })
@@ -50,9 +50,36 @@ const HabitCard = ({ habit, currentDay }) => {
         getTotalCheckins()
     }, [getTotalCheckins]);
 
+    const handleClick = async () => {
+        const formattedDate = new Date(currentDay.getTime() - (currentDay.getTimezoneOffset() * 60 * 1000)).toISOString().split('T')[0]
+        fetch('http://127.0.0.1:5000/api/custom_habits_checkins/',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                credentials: 'include',
+                body: JSON.stringify({
+                    for_date: formattedDate,
+                    habit_id: habit.habit_id
+                })
+            }
+        ).then((res) => {
+            if (res.status === 200 && res.ok) {
+                // update counts
+                setDailyCount(dailyCount + 1)
+                setTotalCount(totalCount + 1)
+            } else if (res.status === 400) {
+                console.log('error')
+            }
+        }).catch((e) => {
+            console.log(e.message)
+        })
+    }
+
     return (
         <div className='w-full flex flex-row flex-nowrap items-center justify-between px-3 py-2 gap-4 rounded-full cursor-pointer bg-appGray-1'>
-            <div className='rounded-full border border-appGreen bg-appWhite hover:bg-appGreen hover:text-appWhite cursor-pointer button-animation px-3 py-1'>{dailyCount}</div>
+            <div className='rounded-full border border-appGreen bg-appWhite hover:bg-appGreen hover:text-appWhite cursor-pointer button-animation px-3 py-1' onClick={handleClick}>{dailyCount}</div>
             <div className='flex-1 truncate'>{habit.habit_name}</div>
             <div className='text-xs text-appGray-3 pr-2 font-bold'>Total: {totalCount}</div>
         </div>
