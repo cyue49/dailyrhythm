021510974feb886794efeb6e0ad9a15533116003
Chen Yue <yuechen049@gmail.com>
2024-08-09 22:58:28 -0400

    refactored api calls for checkins into a separate CheckinServices file

--- frontend/src/components/myhabits/HabitCard.js
+++ frontend/src/components/myhabits/HabitCard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { formatDate, formatDateMonthStart, formatDateWeekAgo } from '../../utils/DateUtils'
+import { getCountBetween, getDayCount, getTotalCount, incrementCheckin } from '../../services/CheckinServices'
 
 const HabitCard = ({ habit, currentDay }) => {
     // states for habit checkin counts
@@ -11,22 +12,9 @@ const HabitCard = ({ habit, currentDay }) => {
 
     // fetch daily checkin count
     const getDailyCheckins = useCallback(() => {
-        // get current date in yyyy-mm-dd format
-        const newDay = formatDate(currentDay)
-
-        // fetch single day checkin count
-        fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit.habit_id}/count/day/${newDay}`, { credentials: 'include' })
-            .then((res) => {
-                if (res.status === 200 && res.ok) {
-                    res.json()
-                        .then((data) => {
-                            setDailyCount(parseInt(data.count))
-                        })
-                }
-            })
-            .catch((e) => {
-                console.log(e.message)
-            })
+        const today = formatDate(currentDay)
+        getDayCount(habit.habit_id, today)
+            .then(response => setDailyCount(response))
     }, [currentDay, habit.habit_id]);
 
     useEffect(() => {
@@ -35,23 +23,10 @@ const HabitCard = ({ habit, currentDay }) => {
 
     // fetch weekly checkin count
     const getWeeklyCheckins = useCallback(() => {
-        // today and week ago date
-        const today = formatDate(currentDay)
+        const today = formatDate(new Date())
         const weekAgo = formatDateWeekAgo(currentDay)
-
-        // fetch single day checkin count
-        fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit.habit_id}/count/from/${weekAgo}/to/${today}`, { credentials: 'include' })
-            .then((res) => {
-                if (res.status === 200 && res.ok) {
-                    res.json()
-                        .then((data) => {
-                            setWeeklyCount(parseInt(data.count))
-                        })
-                }
-            })
-            .catch((e) => {
-                console.log(e.message)
-            })
+        getCountBetween(habit.habit_id, weekAgo, today)
+            .then(response => setWeeklyCount(response))
     }, [currentDay, habit.habit_id]);
 
     useEffect(() => {
@@ -60,23 +35,10 @@ const HabitCard = ({ habit, currentDay }) => {
 
     // fetch monthly checkin count
     const getMonthlyCheckins = useCallback(() => {
-        // today and start of month 
-        const today = formatDate(currentDay)
+        const today = formatDate(new Date())
         const monthStart = formatDateMonthStart(currentDay)
-
-        // fetch single day checkin count
-        fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit.habit_id}/count/from/${monthStart}/to/${today}`, { credentials: 'include' })
-            .then((res) => {
-                if (res.status === 200 && res.ok) {
-                    res.json()
-                        .then((data) => {
-                            setMonthlyCount(parseInt(data.count))
-                        })
-                }
-            })
-            .catch((e) => {
-                console.log(e.message)
-            })
+        getCountBetween(habit.habit_id, monthStart, today)
+            .then(response => setMonthlyCount(response))
     }, [currentDay, habit.habit_id]);
 
     useEffect(() => {
@@ -85,50 +47,29 @@ const HabitCard = ({ habit, currentDay }) => {
 
     // fetch total checkin count
     const getTotalCheckins = useCallback(() => {
-        // fetch count
-        fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit.habit_id}/count`, { credentials: 'include' })
-            .then((res) => {
-                if (res.status === 200 && res.ok) {
-                    res.json()
-                        .then((data) => {
-                            setTotalCount(parseInt(data.count))
-                        })
-                }
-            })
-            .catch((e) => {
-                console.log(e.message)
-            })
+        getTotalCount(habit.habit_id)
+            .then(response => setTotalCount(response))
     }, [habit.habit_id]);
 
     useEffect(() => {
         getTotalCheckins()
     }, [getTotalCheckins]);
 
+    // handle clicking on daily count circle to increment checkin count
     const handleClick = async () => {
-        const formattedDate = new Date(currentDay.getTime() - (currentDay.getTimezoneOffset() * 60 * 1000)).toISOString().split('T')[0]
-        fetch('http://127.0.0.1:5000/api/custom_habits_checkins/',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                credentials: 'include',
-                body: JSON.stringify({
-                    for_date: formattedDate,
-                    habit_id: habit.habit_id
-                })
-            }
-        ).then((res) => {
-            if (res.status === 200 && res.ok) {
-                // update counts
-                setDailyCount(dailyCount + 1)
-                setTotalCount(totalCount + 1)
-            } else if (res.status === 400) {
-                console.log('error')
-            }
-        }).catch((e) => {
-            console.log(e.message)
+        const data = JSON.stringify({
+            for_date: formatDate(currentDay),
+            habit_id: habit.habit_id
         })
+        incrementCheckin(data)
+            .then(response => {
+                if (response === 1) {
+                    setDailyCount(dailyCount + 1)
+                    setWeeklyCount(weeklyCount + 1)
+                    setMonthlyCount(monthlyCount + 1)
+                    setTotalCount(totalCount + 1)
+                }
+            })
     }
 
     const navigate = useNavigate()
@@ -141,9 +82,9 @@ const HabitCard = ({ habit, currentDay }) => {
     }
 
     return (
-        <div className='w-full flex flex-row flex-nowrap items-center justify-between px-3 py-2 gap-4 rounded-full cursor-pointer bg-appGray-1' onClick={handleNavigate}>
+        <div className='w-full flex flex-row flex-nowrap items-center justify-between px-3 py-2 gap-4 rounded-full bg-appGray-1'>
             <div className='rounded-full border border-appGreen bg-appWhite hover:bg-appGreen hover:text-appWhite cursor-pointer button-animation px-3 py-1' onClick={handleClick}>{dailyCount}</div>
-            <div className='flex-1 truncate'>{habit.habit_name}</div>
+            <div className='flex-1 truncate cursor-pointer' onClick={handleNavigate}>{habit.habit_name}</div>
             <div className='text-xs text-appGray-3 pr-2 font-bold'>Total: {totalCount}</div>
         </div>
     )
---
+++ frontend/src/services/CheckinServices.js
@@ -0,0 +1,61 @@
+// get checkin count for a habit for a day
+export const getDayCount = (habit_id, date) => {
+    return fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit_id}/count/day/${date}`, { credentials: 'include' })
+        .then((res) => res.json())
+        .then((data) => {
+            return parseInt(data.count)
+        })
+        .catch((e) => {
+            console.log(e.message);
+            return 0
+        })
+}
+
+// get checkin count for a habit between two dates
+export const getCountBetween = (habit_id, startDate, endDate) => {
+    return fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit_id}/count/from/${startDate}/to/${endDate}`, { credentials: 'include' })
+        .then((res) => res.json())
+        .then((data) => {
+            return parseInt(data.count)
+        })
+        .catch((e) => {
+            console.log(e.message);
+            return 0
+        })
+}
+
+// get total checkin counts for a habit
+export const getTotalCount = (habit_id) => {
+    return fetch(`http://127.0.0.1:5000/api/custom_habits_checkins/habit/${habit_id}/count`, { credentials: 'include' })
+        .then((res) => res.json())
+        .then((data) => {
+            return parseInt(data.count)
+        })
+        .catch((e) => {
+            console.log(e.message);
+            return 0
+        })
+}
+
+// make a post request to create a new checkin 
+export const incrementCheckin = (data) => {
+    return fetch('http://127.0.0.1:5000/api/custom_habits_checkins/',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: data
+        }
+    ).then((res) => {
+        if (res.status === 200 && res.ok) {
+            return 1
+        } else if (res.status === 400) {
+            return 0
+        }
+    }).catch((e) => {
+        console.log(e.message)
+        return 0
+    })
+} 
