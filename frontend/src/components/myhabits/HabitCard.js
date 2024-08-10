59ec84afc13976bbcba4616d3ac1b94520232163
Chen Yue <yuechen049@gmail.com>
2024-08-10 00:31:04 -0400

    updated Habit Card

--- frontend/src/components/myhabits/HabitCard.js
+++ frontend/src/components/myhabits/HabitCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { formatDate, formatDateMonthStart, formatDateWeekAgo } from '../../utils/DateUtils'
 import { getCountBetween, getDayCount, getTotalCount, incrementCheckin } from '../../services/CheckinServices'
@@ -11,52 +11,36 @@ const HabitCard = ({ habit, currentDay }) => {
     const [totalCount, setTotalCount] = useState(0)
 
     // fetch daily checkin count
-    const getDailyCheckins = useCallback(() => {
+    useEffect(() => {
         const today = formatDate(currentDay)
         getDayCount(habit.habit_id, today)
             .then(response => setDailyCount(response))
     }, [currentDay, habit.habit_id]);
 
-    useEffect(() => {
-        getDailyCheckins()
-    }, [getDailyCheckins]);
-
     // fetch weekly checkin count
-    const getWeeklyCheckins = useCallback(() => {
+    useEffect(() => {
         const today = formatDate(new Date())
         const weekAgo = formatDateWeekAgo(currentDay)
         getCountBetween(habit.habit_id, weekAgo, today)
             .then(response => setWeeklyCount(response))
     }, [currentDay, habit.habit_id]);
 
-    useEffect(() => {
-        getWeeklyCheckins()
-    }, [getWeeklyCheckins]);
-
     // fetch monthly checkin count
-    const getMonthlyCheckins = useCallback(() => {
+    useEffect(() => {
         const today = formatDate(new Date())
         const monthStart = formatDateMonthStart(currentDay)
         getCountBetween(habit.habit_id, monthStart, today)
             .then(response => setMonthlyCount(response))
     }, [currentDay, habit.habit_id]);
 
-    useEffect(() => {
-        getMonthlyCheckins()
-    }, [getMonthlyCheckins]);
-
     // fetch total checkin count
-    const getTotalCheckins = useCallback(() => {
+    useEffect(() => {
         getTotalCount(habit.habit_id)
             .then(response => setTotalCount(response))
     }, [habit.habit_id]);
 
-    useEffect(() => {
-        getTotalCheckins()
-    }, [getTotalCheckins]);
-
     // handle clicking on daily count circle to increment checkin count
-    const handleClick = async () => {
+    const handleClick = () => {
         const data = JSON.stringify({
             for_date: formatDate(currentDay),
             habit_id: habit.habit_id
