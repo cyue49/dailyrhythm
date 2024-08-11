64cd6414209eb4ba83ee22c25cd4d219b2196082
Chen Yue <yuechen049@gmail.com>
2024-08-11 02:28:18 -0400

    added deleting a category form My Habits page

--- frontend/src/components/myhabits/CategoryDivider.js
+++ frontend/src/components/myhabits/CategoryDivider.js
@@ -6,6 +6,7 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import CategoryDialog from '../myhabits/CategoryDialog'
+import { deleteCategory } from '../../services/CategoryServices'
 
 const CategoryDivider = ({ category, currentDay }) => {
     const [habits, setHabits] = useState([])
@@ -18,9 +19,15 @@ const CategoryDivider = ({ category, currentDay }) => {
             .then(response => setHabits(response))
     }, [category.category_id]);
 
-    // handle rename
-
     // handle delete
+    const handleDelete = () => {
+        deleteCategory(category.category_id)
+        .then(response => {
+            if (response === 1) {
+                // todo
+            }
+        })
+    }
 
     return (
         <div className='center-of-div flex-col flex-nowrap w-full gap-1'>
@@ -32,11 +39,11 @@ const CategoryDivider = ({ category, currentDay }) => {
                         anchor="right"
                         className='bg-appWhite border border-appGreen rounded-2xl m-2 w-[150px] flex flex-col overflow-hidden transition duration-200 ease-in-out [--anchor-gap:var(--spacing-5)] data-[closed]:-translate-y-1 data-[closed]:opacity-0'>
                         <div className='px-4 pt-2 pb-1 center-of-div hover:bg-appGreen hover:text-appWhite button-animation cursor-pointer' onClick={() => setDialogOpen(true)}>Rename</div>
-                        <div className='px-4 pt-1 pb-2 center-of-div hover:bg-appGreen hover:text-appWhite button-animation cursor-pointer'>Delete</div>
+                        <div className='px-4 pt-1 pb-2 center-of-div hover:bg-appGreen hover:text-appWhite button-animation cursor-pointer' onClick={handleDelete}>Delete</div>
                     </PopoverPanel>
                 </Popover>
                 <div className='text-appGreen font-bold'>{category.category_name.toString()}</div>
-                <div className='center-of-div h-[2px] bg-appGreen flex-1'> </div>
+                <div className='center-of-div h-[1px] bg-appGreen flex-1'> </div>
                 <div className='text-appGreen font-bold'>{habits.length}</div>
             </div>
             {habits.map((habit, index) => (
