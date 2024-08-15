67a7fc7fcdeb457fe5428c5ae957a53bb4bdccc1
Chen Yue <yuechen049@gmail.com>
2024-08-15 00:44:29 -0400

    updated the styling for email verification page

--- frontend/src/pages/EmailVerification.js
+++ frontend/src/pages/EmailVerification.js
@@ -5,8 +5,9 @@ import { verifyEmail } from '../services/AuthServices'
 const EmailVerification = () => {
     const [isVerified, setisVerified] = useState(false)
     const [message, setMessage] = useState('Verifying email.')
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
 
+    const navigate = useNavigate()
 
     useEffect(() => {
         const token = searchParams.get('token')
@@ -16,7 +17,7 @@ const EmailVerification = () => {
                 console.log(response)
                 if (response === 'success') {
                     setisVerified(true)
-                    setMessage('Your email account has been verified! Click on the following button to go back to the app!')
+                    setMessage('Your email account has been verified! Please refresh your app or click on the following button to go back to the app!')
                 } else if (response === 'expired') {
                     setMessage('Your email verification link has expired. Please send a new verification email from your profile page.')
                 } else {
@@ -25,12 +26,16 @@ const EmailVerification = () => {
             })
     }, [searchParams])
 
+    const navigateTo = () => { navigate('/myhabits') }
     return (
-        <div className='center-of-div flex-col gap-4'>
-            <div>{message}</div>
-            {
-                isVerified ? <div>Show button here</div> : null
-            }
+        <div className='w-screen h-screen center-of-div bg-primaryColor text-primaryTextColor'>
+            <div className='w-11/12 h-fit max-w-[450px] max-h-fit min-w-80 min-h-96 p-8 rounded-3xl bg-mainCardColor center-of-div flex-col gap-8'>
+                <div className='text-center'>{message}</div>
+                {
+                    isVerified ? <div className='primary-color-button center-of-div' onClick={navigateTo}>Go to Daily Rhythm</div> : null
+                }
+            </div>
+
         </div>
     )
 }
